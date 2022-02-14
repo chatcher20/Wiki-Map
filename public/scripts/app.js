c0961f068255bf4f98c38899dd15a022c9f8cd20
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-02-14 12:31:52 -0800

    Initial partials and style reset

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,10 @@
 // Client facing scripts here
-
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  map = new google.maps.Map(document.getElementById("map"),
+  {
+    center: { lat: 49.28741410202669, lng: -123.10724415091286 },
+    zoom: 14,
   });
-}
-
+};
