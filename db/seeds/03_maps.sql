ab4ed05bc63b32200b848b86e9679a592dd20861
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-02-17 18:52:55 -0800

    Added maps to database, adds index favourite/remove buttons

--- db/seeds/03_maps.sql
+++ db/seeds/03_maps.sql
@@ -1,1 +1,14 @@
-INSERT INTO maps (map_name) VALUES ('This test map');
+INSERT INTO maps (map_name, is_favourite, has_contributed, user_id)
+VALUES ('Best places to eat', 'true', 'false', 1);
+
+INSERT INTO maps (map_name, is_favourite, has_contributed, user_id)
+VALUES ('Hangout Spots', 'false', 'false', 1);
+
+INSERT INTO maps (map_name, is_favourite, has_contributed, user_id)
+VALUES ('Avoid these areas', 'false', 'false', 2);
+
+INSERT INTO maps (map_name, is_favourite, has_contributed, user_id)
+VALUES ('Dessert spots', 'true', 'false', 1);
+
+INSERT INTO maps (map_name, is_favourite, has_contributed, user_id)
+VALUES ('Free parking!', 'true', 'false', 2);
