bf87a19fb92df4b2f85ac41c17eb177dcb6682ed
Chris Hatcher <hatcherchris20@gmail.com>
2022-02-18 01:16:06 +0000

    adding example pics and example pin object

---
+++ db/seeds/02_pins.sql
@@ -0,0 +1,4 @@
+INSERT INTO pins (title, description, image) VALUES ('Crystal Falls', 'Waterfalls located in Coquitlam, BC', 'this is a test entry into pins database');
+
+
+-- INSERT INTO users (first_name, last_name, is_authenticated) VALUES ('Kira', 'Kong', false);
